d134d54a2885a2a0a92a92b70cd60c1a2ba10e90
Anas-Emmanuel FAOUR <anas-emmanuel.faour@orange.fr>
2020-07-15 11:09:36 +0200

    init project

---
+++ WpfAppVideo/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfAppVideo.Views;
+
+namespace WpfAppVideo
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //Afficher la fenetre pour s'authentifier
+            Authentifier();
+        }
+
+        private void Authentifier()
+        {
+            // Masquer la fenetre courante (this)
+            // Afficher la fenetre d'authentifiaction
+            this.Hide();
+            Authentification1 authentification = new Authentification1();
+            // Afficher authenitifaction en tant que une boîte de dialogue
+            // qui retourne TRUE si l'authentification est réussi
+            authentification.Visibility = Visibility.Visible;
+        }
+
+    }
+}
